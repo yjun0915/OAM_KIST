fc801891b324ed1aea67ac3103d026d46052f002
yjun0915 <kyjun09150916@gmail.com>
2025-12-29 00:25:04 +0900

    release: v0.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="OAM_KIST",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     install_requires=[
         "numpy",
@@ -18,5 +18,9 @@ setup(
     description="Quantum information and technology using OAM states and SLM for KIST research",
     long_description=long_description,
     long_description_content_type='text/markdown',
+    project_urls={
+        "Documentation": "https://oam-kist.readthedocs.io",
+        "Source": "https://github.com/yjun0915/OAM_KIST",
+    },
     python_requires=">=3.7",
 )
