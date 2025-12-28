fa65e5be31cee4f702e80b9e883ba441eda835c0
yjun0915 <kyjun09150916@gmail.com>
2025-12-28 19:50:00 +0900

    docs: add docstyle for hologram.py
    google style의 docstyle를 모든 함수에 적용하였습니다.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="OAM_KIST",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "numpy",
@@ -10,7 +10,7 @@ setup(
         "scipy",
     ],
     author="Youngjun Kim",
-    author_email="kyjun0915@kist.re.kr",z
+    author_email="kyjun0915@kist.re.kr",
     description="Quantum information and technology using OAM states and SLM for KIST research",
     python_requires=">=3.7",
 )
