b6f102136fc38124e2a0590ca40ce5b4bb5237e1
yjun0915 <kyjun09150916@gmail.com>
2026-05-22 16:34:25 +0900

    feat: arg update

--- OAM_KIST/holography.py
+++ OAM_KIST/holography.py
@@ -7,7 +7,7 @@ from scipy.special import factorial, eval_genlaguerre
 from .utils import inv_sinc, inv_sinc_minus, diffraction
 
 
-def generate_oam_superposition(res, pixel_pitch, beam_w0, l_modes, p_modes, weights, prepare=True, measure=False):
+def generate_oam_superposition(res, pixel_pitch, beam_w0, l_modes, p_modes, weights, prepare=True, measure=False, displacement=[0, 0]):
     """ Creat E field of superimposed OAM mode (Interferogram method)
 
     Args:
@@ -34,8 +34,8 @@ def generate_oam_superposition(res, pixel_pitch, beam_w0, l_modes, p_modes, weig
         >>> superposition_args = [slm_res, slm_pitch, input_w0, target_l, target_p, target_weights, True, False]
         >>> fields = generate_oam_superposition(*superposition_args)
     """
-    x = np.linspace(-res[0] * pixel_pitch / 2, (res[0]-1) * pixel_pitch / 2, res[0])
-    y = np.linspace(-res[1] * pixel_pitch / 2, (res[1]-1) * pixel_pitch / 2, res[1])
+    x = np.linspace(-res[0] * pixel_pitch / 2, (res[0]-1) * pixel_pitch / 2, res[0]) + (displacement[0] * pixel_pitch)
+    y = np.linspace(-res[1] * pixel_pitch / 2, (res[1]-1) * pixel_pitch / 2, res[1]) + (displacement[1] * pixel_pitch)
     X, Y = np.meshgrid(x, y)
 
     R = np.sqrt(X ** 2 + Y ** 2)
